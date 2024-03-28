a21c76c09b4d9316135a02b10b6bfb00521520f8
student <student@DESKTOP-MO92V74>
2024-03-28 12:58:45 +0200

    Add design

--- FirestationSystem/View/LoginView.Designer.cs
+++ FirestationSystem/View/LoginView.Designer.cs
@@ -28,70 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Label = new System.Windows.Forms.Label();
             this.LoginButton = new System.Windows.Forms.Button();
             this.PasswordTextBox = new System.Windows.Forms.TextBox();
             this.UsernameTextBox = new System.Windows.Forms.TextBox();
             this.RegisterViewButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // Label
-            // 
-            this.Label.AutoSize = true;
-            this.Label.Location = new System.Drawing.Point(68, 9);
-            this.Label.Name = "Label";
-            this.Label.Size = new System.Drawing.Size(33, 13);
-            this.Label.TabIndex = 7;
-            this.Label.Text = "Login";
             // 
             // LoginButton
             // 
-            this.LoginButton.Location = new System.Drawing.Point(50, 106);
+            this.LoginButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.LoginButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.LoginButton.Location = new System.Drawing.Point(13, 113);
+            this.LoginButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.Size = new System.Drawing.Size(140, 28);
             this.LoginButton.TabIndex = 6;
             this.LoginButton.Text = "login";
-            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.UseVisualStyleBackColor = false;
             this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
             // 
             // PasswordTextBox
             // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(9, 78);
+            this.PasswordTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(223)))), ((int)(((byte)(222)))));
+            this.PasswordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PasswordTextBox.Location = new System.Drawing.Point(13, 83);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(162, 20);
+            this.PasswordTextBox.Size = new System.Drawing.Size(215, 22);
             this.PasswordTextBox.TabIndex = 5;
             // 
             // UsernameTextBox
             // 
             this.UsernameTextBox.AccessibleDescription = "";
             this.UsernameTextBox.AccessibleName = "";
-            this.UsernameTextBox.Location = new System.Drawing.Point(9, 41);
+            this.UsernameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(223)))), ((int)(((byte)(222)))));
+            this.UsernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UsernameTextBox.Location = new System.Drawing.Point(13, 33);
+            this.UsernameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UsernameTextBox.Name = "UsernameTextBox";
-            this.UsernameTextBox.Size = new System.Drawing.Size(162, 20);
+            this.UsernameTextBox.Size = new System.Drawing.Size(215, 22);
             this.UsernameTextBox.TabIndex = 4;
             // 
             // RegisterViewButton
             // 
-            this.RegisterViewButton.Location = new System.Drawing.Point(202, 106);
+            this.RegisterViewButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.RegisterViewButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RegisterViewButton.Location = new System.Drawing.Point(162, 113);
+            this.RegisterViewButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.RegisterViewButton.Name = "RegisterViewButton";
-            this.RegisterViewButton.Size = new System.Drawing.Size(59, 23);
+            this.RegisterViewButton.Size = new System.Drawing.Size(66, 28);
             this.RegisterViewButton.TabIndex = 8;
             this.RegisterViewButton.Text = "Register";
-            this.RegisterViewButton.UseVisualStyleBackColor = true;
+            this.RegisterViewButton.UseVisualStyleBackColor = false;
             this.RegisterViewButton.Click += new System.EventHandler(this.RegisterViewButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(213)))), ((int)(((byte)(212)))));
+            this.label2.Location = new System.Drawing.Point(9, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(213)))), ((int)(((byte)(212)))));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Username:";
             // 
             // LoginView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 135);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(62)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(241, 157);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.RegisterViewButton);
-            this.Controls.Add(this.Label);
             this.Controls.Add(this.LoginButton);
             this.Controls.Add(this.PasswordTextBox);
             this.Controls.Add(this.UsernameTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LoginView";
-            this.Text = "LoginView";
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.LoginView_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -99,11 +132,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Label;
         private System.Windows.Forms.Button LoginButton;
         private System.Windows.Forms.TextBox PasswordTextBox;
         private System.Windows.Forms.TextBox UsernameTextBox;
         private System.Windows.Forms.Button RegisterViewButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FirestationSystem/View/RegisterView.Designer.cs
+++ FirestationSystem/View/RegisterView.Designer.cs
@@ -31,77 +31,134 @@
             this.UsernameTextBox = new System.Windows.Forms.TextBox();
             this.PasswordTextBox = new System.Windows.Forms.TextBox();
             this.RegisterButton = new System.Windows.Forms.Button();
-            this.Label = new System.Windows.Forms.Label();
             this.RegisterViewButton = new System.Windows.Forms.Button();
             this.cbmTeamBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // UsernameTextBox
             // 
             this.UsernameTextBox.AccessibleDescription = "";
             this.UsernameTextBox.AccessibleName = "";
-            this.UsernameTextBox.Location = new System.Drawing.Point(53, 41);
+            this.UsernameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(223)))), ((int)(((byte)(222)))));
+            this.UsernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UsernameTextBox.Location = new System.Drawing.Point(10, 34);
+            this.UsernameTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.UsernameTextBox.Name = "UsernameTextBox";
-            this.UsernameTextBox.Size = new System.Drawing.Size(162, 20);
+            this.UsernameTextBox.Size = new System.Drawing.Size(215, 22);
             this.UsernameTextBox.TabIndex = 0;
             // 
             // PasswordTextBox
             // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(53, 78);
+            this.PasswordTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(223)))), ((int)(((byte)(222)))));
+            this.PasswordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PasswordTextBox.Location = new System.Drawing.Point(10, 84);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(162, 20);
+            this.PasswordTextBox.Size = new System.Drawing.Size(215, 22);
             this.PasswordTextBox.TabIndex = 1;
             // 
             // RegisterButton
             // 
-            this.RegisterButton.Location = new System.Drawing.Point(94, 106);
+            this.RegisterButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.RegisterButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RegisterButton.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RegisterButton.Location = new System.Drawing.Point(10, 120);
+            this.RegisterButton.Margin = new System.Windows.Forms.Padding(4);
             this.RegisterButton.Name = "RegisterButton";
-            this.RegisterButton.Size = new System.Drawing.Size(75, 23);
+            this.RegisterButton.Size = new System.Drawing.Size(215, 28);
             this.RegisterButton.TabIndex = 2;
             this.RegisterButton.Text = "register";
-            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.UseVisualStyleBackColor = false;
             this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
-            // 
-            // Label
-            // 
-            this.Label.AutoSize = true;
-            this.Label.Location = new System.Drawing.Point(112, 9);
-            this.Label.Name = "Label";
-            this.Label.Size = new System.Drawing.Size(46, 13);
-            this.Label.TabIndex = 3;
-            this.Label.Text = "Register";
             // 
             // RegisterViewButton
             // 
-            this.RegisterViewButton.Location = new System.Drawing.Point(308, 106);
+            this.RegisterViewButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.RegisterViewButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RegisterViewButton.Font = new System.Drawing.Font("Comic Sans MS", 9F);
+            this.RegisterViewButton.Location = new System.Drawing.Point(233, 120);
+            this.RegisterViewButton.Margin = new System.Windows.Forms.Padding(4);
             this.RegisterViewButton.Name = "RegisterViewButton";
-            this.RegisterViewButton.Size = new System.Drawing.Size(48, 23);
+            this.RegisterViewButton.Size = new System.Drawing.Size(160, 28);
             this.RegisterViewButton.TabIndex = 4;
             this.RegisterViewButton.Text = "Login";
-            this.RegisterViewButton.UseVisualStyleBackColor = true;
+            this.RegisterViewButton.UseVisualStyleBackColor = false;
             this.RegisterViewButton.Click += new System.EventHandler(this.RegisterViewButton_Click);
             // 
             // cbmTeamBox
             // 
+            this.cbmTeamBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(223)))), ((int)(((byte)(222)))));
+            this.cbmTeamBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cbmTeamBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbmTeamBox.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbmTeamBox.ForeColor = System.Drawing.Color.Black;
             this.cbmTeamBox.FormattingEnabled = true;
-            this.cbmTeamBox.Location = new System.Drawing.Point(235, 40);
+            this.cbmTeamBox.Location = new System.Drawing.Point(233, 32);
+            this.cbmTeamBox.Margin = new System.Windows.Forms.Padding(4);
             this.cbmTeamBox.Name = "cbmTeamBox";
-            this.cbmTeamBox.Size = new System.Drawing.Size(121, 21);
+            this.cbmTeamBox.Size = new System.Drawing.Size(160, 26);
+            this.cbmTeamBox.Sorted = true;
             this.cbmTeamBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(213)))), ((int)(((byte)(212)))));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(213)))), ((int)(((byte)(212)))));
+            this.label2.Location = new System.Drawing.Point(6, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(213)))), ((int)(((byte)(212)))));
+            this.label3.Location = new System.Drawing.Point(229, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Team:";
             // 
             // RegisterView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(368, 141);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(62)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(406, 163);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cbmTeamBox);
             this.Controls.Add(this.RegisterViewButton);
-            this.Controls.Add(this.Label);
             this.Controls.Add(this.RegisterButton);
             this.Controls.Add(this.PasswordTextBox);
             this.Controls.Add(this.UsernameTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "RegisterView";
-            this.Text = "Form1";
+            this.Text = "Register";
             this.Load += new System.EventHandler(this.RegisterView_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -113,9 +170,11 @@
         private System.Windows.Forms.TextBox UsernameTextBox;
         private System.Windows.Forms.TextBox PasswordTextBox;
         private System.Windows.Forms.Button RegisterButton;
-        private System.Windows.Forms.Label Label;
         private System.Windows.Forms.Button RegisterViewButton;
         private System.Windows.Forms.ComboBox cbmTeamBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
